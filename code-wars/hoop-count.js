1ef065121398f6ebf795d00bcd78fbd6c4d6f962
Anggaysm <prasetiaangga28@gmail.com>
2021-11-13 21:46:24 +0700

    add code

---
+++ code-wars/hoop-count.js
@@ -0,0 +1,11 @@
+function hoopCount (n) {
+    if(n > 10){
+        return "Great, now move on to tricks"
+    }else{
+        return "Keep at it until you get it"
+    }  
+ }
+ console.log(hoopCount(10))
+ //Keep at it until you get it
+ console.log(hoopCount(11))
+ //Great, now move on to tricks
