6cd61538e61b6a6cdb70873c489709c373abdf30
Anggaysm <prasetiaangga28@gmail.com>
2021-11-13 21:50:57 +0700

    add code

--- code-wars/hoop-count.js
+++ code-wars/hoop-count.js
@@ -1,11 +1,13 @@
 function hoopCount (n) {
     if(n > 10){
         return "Great, now move on to tricks"
-    }else{
+    }else if(n < 10){
         return "Keep at it until you get it"
+    }else{
+      return "Great, now move on to tricks"
     }  
  }
  console.log(hoopCount(10))
  //Keep at it until you get it
- console.log(hoopCount(11))
+ console.log(hoopCount())
  //Great, now move on to tricks
