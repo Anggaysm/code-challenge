a142e2cb1960bdfcf093de35bf69e6d8af8047c6
Anggaysm <prasetiaangga28@gmail.com>
2021-11-24 20:52:41 +0700

    add second ways

--- code-wars/huruf-alay.js
+++ code-wars/huruf-alay.js
@@ -9,3 +9,11 @@ function correct(string)
     
 }
 console.log(correct("L0ND0N"))
+// LONDON
+
+//second ways
+function correct2(str){
+    return str.replace(/0/g, 'O').replace(/5/g, 'S').replace(/1/g, 'I')
+}
+console.log(correct2("1 L0VE YOU"))
+//I LOVE YOU
