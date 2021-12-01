6f820f6ede8e215f75674579f78dd968afb53319
Anggaysm <prasetiaangga28@gmail.com>
2021-12-01 21:02:30 +0700

    add code

---
+++ hacker-rank/mini-max-sum.js
@@ -0,0 +1,17 @@
+function miniMaxSum(arr) {
+    let resultMini = arr[0]
+    let resultMax = arr[0]
+    let sum = 0
+    for(let i = 0; i < 5; i++){
+        if(resultMax < arr[i]){
+            resultMax = arr[i];
+        }else if(resultMini > arr[i]){
+            resultMini = arr[i]
+        }
+        sum += arr[i]
+    }
+    let maxSum = sum - resultMini
+    let miniSum = sum - resultMax
+    console.log(miniSum + " " + maxSum)
+}
+console.log(miniMaxSum([1, 2, 3, 4, 5]))
