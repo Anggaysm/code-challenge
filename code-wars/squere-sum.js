3cb1292bd272dcc0255eb7337a53c81c740c01db
Anggaysm <prasetiaangga28@gmail.com>
2021-12-24 14:29:12 +0700

    add code

---
+++ code-wars/squere-sum.js
@@ -0,0 +1,8 @@
+function squareSum(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i] * numbers[i];
+  }
+  return total;
+}
+console.log(squareSum([1, 2]));
